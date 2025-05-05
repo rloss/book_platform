3faa18986bade423c8eedb44c97ec26343dd947a
rloss <rlossqianss@gmail.com>
2025-05-06 04:31:47 +0900

    다시

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from routers import groups, posts
+from .routers import groups, posts
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
